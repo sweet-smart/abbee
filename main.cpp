c17d1a521d2e4652da89caf9e96fc089cead76ca
sweet-smart <alewidavid178@gmail.com>
2024-09-28 21:39:52 +0100

    first commit

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //string phrase = "Giraffe Academy";
+    //cout << phrase.length();
+
+   // string phrase = "Giraffe Academy";
+   // cout << phrase[2];
+
+   //string phrase = "Giraffe Academy";
+   //phrase [0] = 'B';
+   //cout << phrase;
+
+
+   string phrase = "Giraffe Academy";
+   //cout << phrase.find("Academy", 0);
+   string phrasesub;
+   cout << phrase.substr(8,3);
+   cout << phrasesub;
+
+   return 0;
+
+}
